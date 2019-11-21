3b3e3432eaae08b30b7d1767c6c1fac438f54d7c
Supakoa <41123305+Supakoa@users.noreply.github.com>
2019-11-22 03:10:10 +0700

    koa

--- app/lib/login.page.dart
+++ app/lib/login.page.dart
@@ -12,59 +12,106 @@ class _MyLoginState extends State<MyLogin> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Travel to Phathalung", style: TextStyle(color: Colors.white)),
+          title: Text("Travel to Phathalung",
+              style: TextStyle(color: Colors.white)),
+          backgroundColor: Color.fromARGB(255, 70, 121, 136),
         ),
-        body: Container(
-            color: Colors.green[50],
-            child: Center(
-              child: Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(16),
-                      gradient: LinearGradient(
-                          colors: [Colors.green[100], Colors.green[300]])),
-                  margin: EdgeInsets.all(32),
-                  padding: EdgeInsets.all(24),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[Row(
+        body: ListView(children: <Widget>[
+          Container(
+            height: 200,
+            color: Colors.redAccent,
+          ),
+          Container(
+              color: Colors.green[50],
+              child: Center(
+                child: Container(
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(colors: [
+                      Color.fromARGB(1, 99, 188, 136),
+                      Color.fromARGB(1, 132, 186, 201)
+                    ])),
+                    margin: EdgeInsets.all(0),
+                    padding: EdgeInsets.all(5),
+                    child: Column(
                       mainAxisSize: MainAxisSize.max,
                       mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[Text('เข้าสู่ระบบ',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 30, color: Colors.white),
-                      )],
-                    ),
-                      buildTextFieldEmail(),
-                      buildTextFieldPassword(),
-                      buildButtonSignIn(),
-                    ],
-                  )),
-            )));
+                      children: <Widget>[
+                        Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text('ยินดีตอนรับ',
+                                style: TextStyle(
+                                  fontSize: 55,
+                                  color: Colors.white,
+                                ))
+                          ],
+                        ),
+                        buildTextFieldEmail(),
+                        buildTextFieldPassword(),
+                        buildButtonForget(),
+                        buildButtonSignIn(),
+                      ],
+                    )),
+              )),
+          Container(
+            color: Colors.blue,
+            height: 200,
+          )
+        ]));
   }
 
   Container buildButtonSignIn() {
     return Container(
         constraints: BoxConstraints.expand(height: 50),
-        child: Text("Sign in",
+        child: Text("เข้าสู่ระบบ",
             textAlign: TextAlign.center,
             style: TextStyle(fontSize: 18, color: Colors.white)),
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16), color: Colors.green[200]),
+            borderRadius: BorderRadius.circular(16),
+            color: Color.fromARGB(
+              1,
+              111,
+              162,
+              178,
+            )),
         margin: EdgeInsets.only(top: 16),
         padding: EdgeInsets.all(12));
   }
- 
+
+  Container buildButtonForget() {
+    return Container(
+        constraints: BoxConstraints.expand(
+          height: 50,
+          width: 150,
+        ),
+        child: Text("ลืมรหัสผ่าน ?",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 18, color: Colors.white)),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: Color.fromARGB(
+              1,
+              111,
+              162,
+              178,
+            )),
+        margin: EdgeInsets.only(top: 16),
+        padding: EdgeInsets.all(12));
+  }
+
   Container buildTextFieldEmail() {
     return Container(
         padding: EdgeInsets.all(12),
         decoration: BoxDecoration(
             color: Colors.yellow[50], borderRadius: BorderRadius.circular(16)),
         child: TextField(
-            decoration: InputDecoration.collapsed(hintText: "Email"),
-            style: TextStyle(fontSize: 18)));
+            decoration: InputDecoration.collapsed(
+                hintText: "ชื่อผู้ใช้",
+                hintStyle: TextStyle(fontSize: 18, color: Colors.black)),
+            style: TextStyle(fontSize: 18, color: Colors.black)));
   }
- 
+
   Container buildTextFieldPassword() {
     return Container(
         padding: EdgeInsets.all(12),
@@ -73,8 +120,9 @@ class _MyLoginState extends State<MyLogin> {
             color: Colors.yellow[50], borderRadius: BorderRadius.circular(16)),
         child: TextField(
             obscureText: true,
-            decoration: InputDecoration.collapsed(hintText: "Password"),
-            style: TextStyle(fontSize: 18)));
+            decoration: InputDecoration.collapsed(
+                hintText: "รหัสผ่าน",
+                hintStyle: TextStyle(fontSize: 18, color: Colors.black)),
+            style: TextStyle(fontSize: 18, color: Colors.black)));
   }
-
 }
